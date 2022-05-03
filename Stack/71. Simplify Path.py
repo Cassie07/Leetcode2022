74aa491cd50b4cbbdf55c5b9cba349db93e17d35
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2022-05-02 21:38:34 -0500

    Create 71. Simplify Path.py

---
+++ Stack/71. Simplify Path.py
@@ -0,0 +1,22 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        
+        res = []
+        stack = []
+        for content in path.split('/'):
+            if content == '' or content =='.':
+                continue
+            elif content == '..':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(content)
+        print(stack)
+        
+        s = '' 
+        if stack:
+            for i in stack:
+                s += '/' + i
+            return s
+        else:
+            return '/'
